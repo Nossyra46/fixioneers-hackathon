9141fb62223a5efea9e25e2eddd326e8fc3e1eec
Kenzo Naito <kenzo.naito@edhec.com>
2016-03-12 21:47:41 +0100

    Added react test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,6 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+//= require react
+//= require react_ujs
+//= require components
---
+++ app/assets/javascripts/components/components.js.jsx
@@ -0,0 +1,11 @@
+var Container = React.createClass({
+  render: function () {
+    return (
+      <div>
+        React test.
+      </div>
+    );
+  }
+});
+
+// ReactDOM.render(document.getElementById('sendtext-modal'), <Container />);
